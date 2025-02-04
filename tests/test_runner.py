9d8c4ab8f91ca071417d52d4fa6e86d9826fdd84
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-02-04 16:27:12 +0100

    Add parameter passing to test_runner

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -4,25 +4,42 @@ import sys
 from cocotb.runner import get_runner
 
 
-def main(dut_name, sources):
+def main(dut_name: str, sources: list[str], parameters: dict[str, int]):
     simulator = get_runner("icarus")
     simulator.build(
         sources=sources,
         hdl_toplevel=dut_name,
         clean=True,
-        waves=True
+        waves=True,
+        parameters=parameters
     )
     simulator.test(
         hdl_toplevel=dut_name,
         test_module=dut_name + "_tb",
-        waves=True
+        waves=True,
+        parameters=parameters
     )
 
+
+dependencies = {
+    "Mac": [],
+    "MacManager": ["Mac"],
+    "SystolicMatrixMultiplier": ["Mac", "MacManager"]
+}
 
 if __name__ == "__main__":
     src_path = sys.argv[1]
     dut_name = sys.argv[2]
-    sources = [os.path.join(src_path, sys.argv[i] + ".v")
-               for i in range(2, len(sys.argv))]
 
-    main(dut_name, sources)
+    sources = [dut_name] + dependencies[dut_name]
+    sources = [os.path.join(src_path, s + ".v") for s in sources]
+
+    parameters = dict()
+    for i in range(3, len(sys.argv)):
+        arg = sys.argv[i]
+        k, v = arg.split("=")[0], arg.split("=")[1]
+        # v has to be an int
+        parameters[k] = int(v)
+        os.environ["DUT_" + k] = v
+
+    main(dut_name, sources, parameters)
