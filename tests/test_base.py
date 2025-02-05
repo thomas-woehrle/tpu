dfb2146b9328a3968e4c0fd5f141580061431af9
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-02-05 21:11:13 +0100

    Make TestBase more general

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,62 +2,72 @@ from abc import ABC, abstractmethod
 
 import cocotb
 from cocotb.queue import Queue
-from cocotb.triggers import RisingEdge
+from cocotb.triggers import FallingEdge, RisingEdge
 
 
-class TestBase[ParamsType, InputType, OutputType](ABC):
+class TestBase[ParamsType, DutSnapshotType, InputType](ABC):
     # Potentially move to just a dut type, which also types the self.dut indirectly
     def __init__(self, dut, params: ParamsType, n_checks: int):
         self.dut = dut
         self.params = params
         self.n_checks = n_checks
-        self.transaction_queue = Queue[tuple[InputType, OutputType]]()
+        self.transaction_queue = Queue[tuple[DutSnapshotType, DutSnapshotType]](
+        )
         self.input_queue = Queue[InputType]()
 
     @abstractmethod
-    def get_input_from_dut(self) -> InputType:
+    def get_dut_snapshot(self) -> DutSnapshotType:
         pass
 
     @abstractmethod
-    def get_output_from_dut(self) -> OutputType:
+    async def wait_between_snapshots(self, first_snapshot: DutSnapshotType):
         pass
 
     @abstractmethod
-    async def wait_in_monitor(self, curr_input: InputType):
+    async def score_transaction(self, t: tuple[DutSnapshotType, DutSnapshotType]):
         pass
 
     @abstractmethod
-    async def score_transaction(self, t: tuple[InputType, OutputType]):
+    async def generate_input(self, i: int) -> InputType:
+        # could get counter or sim time as argument
         pass
 
     @abstractmethod
-    async def generate_input(self):
+    async def drive_input(self, next_input: InputType):
+        """After this coroutine finishes, it gets called again at the next FallingEdge"""
         pass
 
-    @abstractmethod
-    async def drive_input(self):
-        pass
-
-    async def monitor(self):
+    async def start_monitor(self):
         await RisingEdge(self.dut.clk)
         while True:
-            input = self.get_input_from_dut()
+            snapshot_0 = self.get_dut_snapshot()
 
-            await self.wait_in_monitor(input)
+            await self.wait_between_snapshots(snapshot_0)
 
-            output = self.get_output_from_dut()
+            snapshot_1 = self.get_dut_snapshot()
 
             # put transaction in queue
-            t = (input, output)
+            t = (snapshot_0, snapshot_1)
             await self.transaction_queue.put(t)
 
-    async def score(self):
+    async def start_scoreboard(self):
         while True:
             t = await self.transaction_queue.get()
             await self.score_transaction(t)
 
+    async def start_input_generator(self):
+        for i in range(self.n_checks):
+            next_input = await self.generate_input(i)
+            await self.input_queue.put(next_input)
+
+    async def start_input_driver(self):
+        while True:
+            await FallingEdge(self.dut.clk)
+            next_input = await self.input_queue.get()
+            await self.drive_input(next_input)
+
     def start_soon(self):
-        cocotb.start_soon(self.drive_input())
-        cocotb.start_soon(self.generate_input())
-        cocotb.start_soon(self.score())
-        cocotb.start_soon(self.monitor())
+        cocotb.start_soon(self.start_input_generator())
+        cocotb.start_soon(self.start_input_driver())
+        cocotb.start_soon(self.start_scoreboard())
+        cocotb.start_soon(self.start_monitor())
