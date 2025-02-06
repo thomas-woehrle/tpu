77b1bc47d839df83cf34f3cd124f59a281431521
Thomas Woehrle <thomas.e.woehrle@gmail.com>
2025-02-06 16:25:07 +0100

    Document and comment test_runner.py and utils.py

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,3 +1,5 @@
+"""Contains functionality to run cocotb tests from the command line
+"""
 import os
 import sys
 
@@ -5,6 +7,14 @@ from cocotb.runner import get_runner
 
 
 def main(dut_name: str, sources: list[str], parameters: dict[str, int]):
+    """Main function to trigger execution of a test
+
+    Args:
+        dut_name: The name of the DUT
+        sources: The paths to the files that are need to build, 
+            including the DUT itself. Fe src/dut.v
+        parameters: Dict representing build parameters
+    """
     simulator = get_runner("icarus")
     simulator.build(
         sources=sources,
@@ -23,6 +33,9 @@ def main(dut_name: str, sources: list[str], parameters: dict[str, int]):
     )
 
 
+# dependencies of the different modules
+# should be handled differently if the number of modules increases
+# fe with separate dependencies file
 dependencies = {
     "Mac": [],
     "MacManager": ["Mac"],
@@ -30,18 +43,25 @@ dependencies = {
 }
 
 if __name__ == "__main__":
+    # read from command line
     src_path = sys.argv[1]
     dut_name = sys.argv[2]
 
+    # create source paths
     sources = [dut_name] + dependencies[dut_name]
     sources = [os.path.join(src_path, s + ".v") for s in sources]
 
+    # read parameters from command line and write them to the environment
+    # so that they are accessible inside the <module_name>_tb.py files
     parameters = dict()
     for i in range(3, len(sys.argv)):
         arg = sys.argv[i]
         k, v = arg.split("=")[0], arg.split("=")[1]
-        # v has to be an int
+
+        # v has to be an int at the moment
         parameters[k] = int(v)
+
+        # the DUT_ prefix is used to find the variables in the test code
         os.environ["DUT_" + k] = v
 
     main(dut_name, sources, parameters)
--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,10 @@
+"""Contains utilities which can be used in the cocotb tests
+
+Contains:
+    * get_params_from_env
+    * pack_matrix_to_int
+    * unpack_binary_value_to_matrix
+"""
 import os
 
 import numpy as np
@@ -5,6 +12,11 @@ from cocotb.binary import BinaryValue
 
 
 def get_params_from_env[ParamType](paramCls: type[ParamType]) -> ParamType:
+    """Accumulates DUT_... environment variables into an object
+
+    Returns:
+        The Parameter Object
+    """
     params = paramCls()
 
     for k, v in os.environ.items():
@@ -15,6 +27,21 @@ def get_params_from_env[ParamType](paramCls: type[ParamType]) -> ParamType:
 
 
 def pack_matrix_to_int(matrix: np.ndarray, op_width: int) -> int:
+    """Packs an np array into a single integer
+
+    Currently only works with positive integers, but can easily be extended
+
+    Args:
+        matrix: The matrix to pack
+        op_width: The op_width to use for each element. Fe if op_width is 8,
+            then 1 becomes 0000_0001
+
+    Raises:
+        ValueError: Raised if a value in the matrix does not fit into the op_width
+
+    Returns:
+        _description_
+    """
     packed_value = 0
     for i, value in enumerate(matrix.flatten()):
         # convert because numpy ints have fixed width
@@ -28,7 +55,20 @@ def pack_matrix_to_int(matrix: np.ndarray, op_width: int) -> int:
 
 
 def unpack_binary_value_to_matrix(packed_value: BinaryValue, matrix_shape: tuple, op_width: int) -> np.ndarray | None:
-    # if the passed packed_value is a string it means it is "x" or "z"
+    """Unpacks a BinaryValue to a matrix
+
+    BinaryValue is the type that signals obtained from a cocotb simulation have.
+
+    Args:
+        packed_value: The value to unpack
+        matrix_shape: The shape of the matrix to unpack to
+        op_width: The operand width of the values. Fe if op_width is 8,
+            then 0001_0001 resolves to 17 and not 1, 1
+
+    Returns:
+        The matrix as np array
+    """
+    # checks whether the packed_value contains "x" or "z"
     if not packed_value.is_resolvable:
         return None
 
